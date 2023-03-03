f40ef30f4715ae7087f4625fb5f37bc9f8c16562
Dmitrii Larionov <dlarionov51@gmail.com>
2023-03-03 21:25:51 +0300

    DataReader implementation

--- DataGenerator/DataEntryIO.py
+++ DataGenerator/DataEntryIO.py
@@ -5,15 +5,35 @@ Created on Fri Mar  3 00:46:22 2023
 @author: SomgBird
 """
 
+import uuid
+import torch
+import dataclasses
+
+from pathlib import Path
 from DataEntry import DataEntry
+from typing import List
 
 
 class DataEntryWriter:
-    def write(self, data_entry : DataEntry, path):
-        pass
+    def write(self, data_entry : DataEntry, path : Path):
+        file_name = uuid.uuid4().hex
+        
+        tensors = []
+        for f in dataclasses.fields(data_entry):
+            tensors.append(getattr(data_entry, f.name))
+        
+        torch.save(tensors, path/file_name)
+        
+    
+    def write_all(self, data_entries : List[DataEntry], path : Path):
+        for d in data_entries:
+            self.write(d, path)
 
 
 class DataEntryReader:
-    def read(self, path) -> DataEntry:
+    def read(self, path : Path) -> DataEntry:
         pass
 
+    
+    def write(self, data_entries : List[DataEntry], path : Path) -> List[DataEntry]:
+        pass
